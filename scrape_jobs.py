cb7f48328ef3c95ed3253bd7f8fb07256ba2a867
Brian Troy <bitroy@yahoo.com>
2021-09-21 17:03:35 -0700

    Added a jobs query to the scape_jobs.py script

--- scrape_jobs.py
+++ scrape_jobs.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
 	url_wafer_process_data = 'https://www.indeed.com/jobs?q=wafer%20process%20data&l=san%20mateo%2C%20ca&start='
 	url_python_developer = 'https://www.indeed.com/jobs?q=python%20developer&l=san%20mateo%2C%20ca&start='
 	url_wafer_defect = 'https://www.indeed.com/jobs?q=wafer%20defect&l=san%20mateo%2C%20ca&start='
-
+	url_wafer_metrology_inspection = 'https://www.indeed.com/jobs?q=wafer%20metrology%20inspection&l=san%20mateo&start='
 	urls = []
 	urls.append(url_wafer_data_inspection)
 	urls.append(url_data_science)
@@ -55,6 +55,8 @@ if __name__ == "__main__":
 	urls.append(url_python_vba)
 	urls.append(url_wafer_process_data)
 	urls.append(url_python_developer)
+	urls.append(url_wafer_defect)
+	urls.append(url_wafer_metrology_inspection)
 
 	for j in range(len(urls)):
 		joblist = []
